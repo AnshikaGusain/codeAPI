bc4fd4079b1cdcd632ca2b8ebe4e98c0bfb4c612
Anshika Gusain <anshikagusain62@gmail.com>
2023-07-15 03:44:48 +0530

    updating port

--- server.js
+++ server.js
@@ -197,8 +197,9 @@ app.get("/",(req,res)=>{
 
 
 // Start the server
-server.listen(process.env.PORT||3000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT=process.env.PORT||3000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
